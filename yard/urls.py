259e2dc315caa15d626367f337379bb2e0f14827
tarmo <tsillajoe@gmail.com>
2022-11-12 00:47:53 +0200

    added detail view for Part

--- yard/urls.py
+++ yard/urls.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 from django.urls import path
 from django.views.generic import TemplateView
+from .views import part_detail_view
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("parts/<int:part_id>/", part_detail_view, name="part_detail"),
 ]
--- yard/views.py
+++ yard/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Part
 
-# Create your views here.
+
+def part_detail_view(request, part_id):
+    part = get_object_or_404(Part, id=part_id)
+    return render(request, "yard/part_detail.html", {"part": part})
