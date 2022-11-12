858bf833cbbdc4cf5049769e4f6e20c1c490e677
tarmo <tsillajoe@gmail.com>
2022-11-12 18:28:34 +0200

    Part instances are editable

--- yard/urls.py
+++ yard/urls.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import part_detail_view, part_create_view
+from .views import part_detail_view, part_create_view, part_edit_view
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
-    path("parts/<int:part_id>/", part_detail_view, name="part_detail"),
-    path("parts/new/", part_create_view, name="part_create"),
+    path("part/<int:part_id>/", part_detail_view, name="part_detail"),
+    path("part/new/", part_create_view, name="part_create"),
+    path("part/<int:part_id>/edit/", part_edit_view, name="part_edit"),
 ]
--- yard/views.py
+++ yard/views.py
@@ -17,3 +17,15 @@ def part_create_view(request):
     else:
         form = PartForm()
     return render(request, "yard/part_edit.html", {"form": form})
+
+
+def part_edit_view(request, part_id):
+    part = get_object_or_404(Part, id=part_id)
+    if request.method == "POST":
+        form = PartForm(request.POST, request.FILES, instance=part)
+        if form.is_valid():
+            part = form.save()
+            return redirect("part_detail", part_id=part.id)
+    else:
+        form = PartForm(instance=part)
+    return render(request, "yard/part_edit.html", {"form": form})
