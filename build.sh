c8d5045029234d0b77a2dcb935ded834142a7390
tarmo <tsillajoe@gmail.com>
2022-12-07 20:20:28 +0200

    added npm and npx commands to build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 set -o errexit
 
 pip install -r requirements.txt
+npm install
+npx tailwindcss -i ./static/src/input.css -o ./static/src/output.css
 
 python manage.py collectstatic --no-input
 python manage.py migrate
