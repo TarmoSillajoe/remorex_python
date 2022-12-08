f1a16bd11b1363820791354e6f0742e60d88d617
tarmo <tsillajoe@gmail.com>
2022-12-08 08:36:48 +0200

    in settings.py, use whitenoise if DEBUG is False

--- remorex/settings.py
+++ remorex/settings.py
@@ -135,9 +135,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STATIC_URL = "/static/"
+
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / "static"
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 STATICFILES_DIRS = [
