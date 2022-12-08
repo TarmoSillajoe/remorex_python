ad9d673d3abdca87ccbbae59476228817709243e
tarmo <tsillajoe@gmail.com>
2022-12-08 08:29:38 +0200

    in settings.py, added STATICFILES_STORAGE

--- remorex/settings.py
+++ remorex/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = "."
-
+STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = "/static/"
 
 
