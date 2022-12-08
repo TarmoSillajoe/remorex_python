568aa2f86ed8cc6cbd26122e2f37619da9748ec8
tarmo <tsillajoe@gmail.com>
2022-12-08 16:13:55 +0200

    settings.py

--- remorex/settings.py
+++ remorex/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 # Default primary key field type
@@ -159,5 +159,5 @@ STATICFILES_FINDERS = (
 )
 
 if not DEBUG:
-    STATIC_ROOT = BASE_DIR / "static"
+    STATIC_ROOT = BASE_DIR / "staticfiles"
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
