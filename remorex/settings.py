f1c3d106963bb230768048cb8306494a9de2757f
tarmo <tsillajoe@gmail.com>
2022-12-08 09:05:16 +0200

    commented out STATICFILES_DIRS

--- remorex/settings.py
+++ remorex/settings.py
@@ -139,13 +139,14 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 if not DEBUG:
-    STATIC_ROOT = BASE_DIR / "staticfiles"
+    STATIC_ROOT = BASE_DIR / "static"
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
