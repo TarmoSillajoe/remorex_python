2b33a8012f37e3900b7bc8439e5b81cf2329e32a
tarmo <tsillajoe@gmail.com>
2022-12-10 04:12:52 +0200

    settings.py

--- remorex/settings.py
+++ remorex/settings.py
@@ -157,5 +157,5 @@ STATICFILES_FINDERS = (
 )
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    STATIC_ROOT = BASE_DIR / "staticfiles"
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
