cb3ad2021fe8696361e195e65307ba50114dbba6
tarmo <tsillajoe@gmail.com>
2022-12-26 00:24:01 +0200

    in build.sh, using poetry

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 # exit on error
 set -o errexit
 
-pip install --upgrade pip
-pip install -r requirements.txt
+poetry install
 
 npm install
 npm run watch
+
 python manage.py collectstatic --no-input
 python manage.py migrate
