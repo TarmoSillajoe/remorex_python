1cde3ff4ce68f374ab7766839f5bf7fe015a860b
tarmo <tsillajoe@gmail.com>
2023-01-21 20:53:24 +0200

    in settings.py, in INSTALLED_APPS using whitenoise.runserver_nostatic

--- remorex/settings.py
+++ remorex/settings.py
@@ -40,6 +40,7 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
