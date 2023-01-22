b1a31e99124e98d9e6844731362fc6de41268688
tarmo <tsillajoe@gmail.com>
2023-01-22 07:13:42 +0200

    separated dev-dependencies to requirements-dev.txt, in yard/admin.py added AssemblyGroup model

--- yard/admin.py
+++ yard/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Car, CarInstance, Part
+from .models import Car, CarInstance, Part, AssemblyGroup
 
 admin.site.register(Car)
 admin.site.register(CarInstance)
 admin.site.register(Part)
+admin.site.register(AssemblyGroup)
