eec3962fc722d887b672d6b92d2712eb812f03c7
tarmo <tsillajoe@gmail.com>
2023-01-22 19:43:40 +0200

    in settings.py STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"

--- remorex/settings.py
+++ remorex/settings.py
@@ -138,8 +138,8 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 if not DEBUG:
     STATIC_ROOT = BASE_DIR / "staticfiles"
-    # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    # STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
     # STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 STATICFILES_FINDERS = [
