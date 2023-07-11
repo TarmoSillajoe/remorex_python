580d736b43767a422286d83ae231ed77365e29c1
tarmo <tsillajoe@gmail.com>
2023-07-11 21:50:46 +0300

    made some translations

--- yard/tests/test_views.py
+++ yard/tests/test_views.py
@@ -7,10 +7,26 @@ class ViewsTestCase(TestCase):
         response = self.client.get("")
         self.assertEqual(response.status_code, 200)
 
-    def test_language_using_cookie(self):
+    def test_language_using_de_cookie(self):
         self.client.cookies.load({settings.LANGUAGE_COOKIE_NAME: "de"})
         response = self.client.get("/")
-        self.assertContains(response, b"Samstag")
+        self.assertContains(response, b"Betriebszeiten")
+
+    def test_language_using_et_cookie(self):
+        self.client.cookies.load({settings.LANGUAGE_COOKIE_NAME: "de"})
+        response = self.client.get("/")
+        self.assertContains(response, b"Liidu liige")
+
+    def test_language_using_en_cookie(self):
+        self.client.cookies.load({settings.LANGUAGE_COOKIE_NAME: "en"})
+        response = self.client.get("/")
+        self.assertContains(response, b"established")
+
+    def test_language_using_ru_cookie(self):
+        self.client.cookies.load({settings.LANGUAGE_COOKIE_NAME: "ru"})
+        response = self.client.get("")
+        decoded_content = response.content.decode("utf-8")
+        self.assertIn("переработчиков", decoded_content)
 
     def test_language_using_header(self):
         response = self.client.get("", headers={"accept-language": "et"})
