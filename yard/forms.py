1286fc786dc25e1c6b5d28712b137e946ea914be
tarmo <tsillajoe@gmail.com>
2023-07-30 19:42:14 +0300

    translated the contact form

--- yard/forms.py
+++ yard/forms.py
@@ -18,7 +18,9 @@ class PartForm(forms.ModelForm):
 
 
 class QueryForm(forms.Form):
-    email = forms.EmailField(required=False)
+    email = forms.EmailField(required=False, label=gettext_lazy("your email"))
     phone = forms.CharField(max_length=20, label=gettext_lazy("your phone"))
-    subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    subject = forms.CharField(required=True, label=gettext_lazy("subject"))
+    message = forms.CharField(
+        widget=forms.Textarea, required=True, label=gettext_lazy("message")
+    )
