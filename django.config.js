87612ac43bdb6e8004f44d49c1100ceddeb43059
Tarmo <tsillajoe@gmail.com>
2023-12-11 12:23:08 +0200

    django.config.js added for pm2

---
+++ django.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "remorex",
+      cwd: process.env.HOME + "domeenid/www.remoreks.ee/remorex_python",
+      script: process.env.HOME + "/.virtualenv/bin/gunicorn",
+      args: "remorex.wsgi:application",
+      interpreter: process.env.HOME + "/.virtualenv/bin/python3.8",
+      max_memory_restart: "128M",
+    },
+  ],
+};
