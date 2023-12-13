64d5a11b5841cb338e7cde3c4c7e79c42036a96c
Tarmo <tsillajoe@gmail.com>
2023-12-13 16:24:19 +0200

    In django.config.js, changed directory name back to old

--- django.config.js
+++ django.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "remorex",
-      cwd: process.env.HOME + "domeenid/www.remoreks.ee/remorex",
+      cwd: process.env.HOME + "domeenid/www.remoreks.ee/remorex_python",
       script: process.env.HOME + "/virtualenv/bin/gunicorn/gunicorn",
       args: "remorex.wsgi:application",
       interpreter: process.env.HOME + "/virtualenv/bin/python3.8",
