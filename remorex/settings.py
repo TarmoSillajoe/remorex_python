da75210901c9a75f2e31629c8b8436eebb659f53
tarmo <tsillajoe@gmail.com>
2024-04-20 14:04:34 +0300

    In settings.py, ixed a typo in AUTH_PASSWORD_VALIDATORS

--- remorex/settings.py
+++ remorex/settings.py
@@ -131,7 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib./.password_validation.CommonPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
